a23fde231056e9125ebf4adada36b04882e5fa99
17602937887 <545192053@qq.com>
2020-03-13 22:20:33 +0800

    code

---
+++ src/cn/Demo/forward/ForwordDemo01.java
@@ -0,0 +1,20 @@
+package cn.Demo.forward;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/forwordDemo01")
+public class ForwordDemo01 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("msg", "Hello");
+        request.getRequestDispatcher("/forwordDemo02").forward(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
